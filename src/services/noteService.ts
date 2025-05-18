cf953713a3aa13139dac18e73b35c8259c13e020
Mahditgo <mahdi90056@gmail.com>
2025-05-18 17:39:13 +0330

    add note service layer

---
+++ src/services/noteService.ts
@@ -0,0 +1,49 @@
+import Note from "../model/noteModel";
+
+
+//create Note
+export const createNote = async ( data : {titel : string, content : string}) => {
+    return await Note.create(data);
+};
+
+//gat all notes
+export const getNotes = async () => {
+    return await Note.findAll();
+};
+
+//get single note by id
+export const getNoteById = async (id: number) => {
+    const note = await Note.findByPk(id);
+    if(!note) {
+        const error = new Error('no note with that id') as any;
+        error.status = 404;
+        return error
+    }
+    return note;
+};
+
+//update note
+export const updateNote = async (id : number, data : {titel ?: string, content ?: string}) => {
+    const note = await Note.findByPk(id);
+    if(!note) {
+        const error = new Error('no note with that Id') as any;
+        error.status = 404;
+        return error;
+    }
+
+    await note.update(data);
+    return note;
+};
+
+//delete note
+export const deleteNote = async (id : number) => {
+     const note = await Note.findByPk(id);
+    if(!note) {
+        const error = new Error('no note with that Id') as any;
+        error.status = 404;
+        return error;
+    }
+
+    await note.destroy();
+    
+}
